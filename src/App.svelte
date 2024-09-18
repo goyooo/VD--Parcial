c0a04cd5bc610e4b8aa42c78bbbdd5b8c5697968
Goyooo <jgoyeneche@mail.utdt.edu>
2024-09-18 04:16:30 -0300

    final

--- src/App.svelte
+++ src/App.svelte
@@ -25,13 +25,13 @@ const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
     {#each numbers as n}
   
     <div class="waffle_wrapper">
-      <p class="cant">{n}</p>
+      <p class="cant">{n}mil</p>
       <div class="waffle">
         
         {#each d3.range(0, 100) as number}
           {#if number < n}
             <div class="cuad lleno">
-              <img class="buevito" src="./public/images/huevo.png" alt="buevito" style="transform: rotate(180deg);">
+              <img class="buevito" src="./src/images/huevo.png" alt="buevito" style="transform: rotate(180deg);">
             </div>
           {:else}
             <div class="cuad vacio"></div>
@@ -56,16 +56,16 @@ const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
 
   <div class="waffles_container">
 
-    {#each numbers as n}
+    {#each numbers1 as n}
   
     <div class="waffle_wrapper">
-      <p class="cant">{n}</p>
+      <p class="cant">{n}mil</p>
       <div class="waffle">
         
         {#each d3.range(0, 100) as number}
           {#if number < n}
             <div class="cuad lleno">
-              <img class="buevito" src="./public/images/huevo.png" alt="buevito" style="transform: rotate(180deg);">
+              <img class="buevito" src="./src/images/huevo.png" alt="buevito" style="transform: rotate(180deg);">
             </div>
           {:else}
             <div class="cuad vacio"></div>
@@ -82,40 +82,41 @@ const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
 
 <p class="titulo-de-graf"> Otras formas de ver los datos</p>
 <div class="graficos-f">
-<div class="flourish-embed flourish-chart" data-src="visualisation/19448565"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/19448565/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
-<div class="flourish-embed flourish-chart" data-src="visualisation/19448458"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/19448458/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
+  <div class="flourish-embed flourish-chart" data-src="visualisation/19448565"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/19448565/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
+  <div class="flourish-embed flourish-chart" data-src="visualisation/19448458"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/19448458/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
 </div>
 
 <div class="footer">
 <p class="name"> Juan Martin Goyeneche</p>
-
-
-
 </div>
 
 <style>
   .footer {
     color:silver;
-    background-color: chocolate;
-    font-size: 20px;
+    text-align: center;
+    background-color: rgb(48, 78, 37);
+    font-size: 30px;
     font-family: 'Trebuchet MS';
-}
+    padding-bottom: 3%;
+    margin-bottom: 0%;
+    
+  }
  .cant {
   text-align: center;
   color:#c0975a;
   margin-bottom: 0%;
-}
+  }
 
-.waffles_container {
+  .waffles_container {
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   gap: 10px;
   background-color: #498a49;
 
-}
+  }
 
-.waffle {
+  .waffle {
   display: grid;
   grid-template-columns: repeat(10, 1fr);
   grid-template-rows: repeat(10, 1fr);
@@ -126,16 +127,19 @@ const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
   gap: 1px;
   background-color: #eee;
   margin-bottom: 10%;
-}
-.cuad {
+  }
+
+  .cuad {
   font-size: 5px;
   width: 10px;
   height: 13px;
-}
-.vacio {
+  }
+
+  .vacio {
   background-color: #ddd;
-}
-.title{
+  }
+
+  .title{
   background-color: #498a49;
   color:#c0975a;
   text-align: center;
@@ -143,49 +147,50 @@ const years = [2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
   font-size: 40px;
   font-family: 'Trebuchet MS';
   margin-bottom:0%;
-}
-.titulo-de-graf {
-text-align: center;
-text-decoration:underline;
-text-decoration-thickness: 2px;
-font-size: 25px;
-font-family: 'Trebuchet MS';
-}
+  }
 
-.graficos-f {
-  display: flex;
-  margin-bottom: 0%;
-  margin-top: 5%;
-}
-
-.flourish-embed  {
-  width: 50%;
-  height: 50%;
-}
-
-.header {
-  font-family: 'Cochin';
-  color: rgb(218, 215, 215);
-  display: flex;
-  flex-direction: column;
+  .titulo-de-graf {
   text-align: center;
-  background-color: rgb(73, 138, 73);
-}
+  text-decoration-thickness: 2px;
+  font-size: 25px;
+  font-family: 'Trebuchet MS';
+  color: #498a49;
+  }
 
-.años {
-  display: flex;
-  justify-content: space-around;
-  background-color: #498a49;
-  font-family:'Trebuchet MS';
-  font-size: 27px;
-  color: #c79d6b;
-  margin-bottom: 0%;   
-}
+  .graficos-f {
+    display: flex;
+    margin-bottom: 0%;
+    margin-top: 5%;
+  }
 
-.maples {
-  margin-top: 0%;
-  display: flex;
-  flex-direction: column;
-  }
+  .flourish-embed  {
+    width: 50%;
+    height: 50%;
+  }
+
+  .header {
+    font-family: 'Cochin';
+    color: rgb(218, 215, 215);
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    background-color: rgb(73, 138, 73);
+  }
+
+  .años {
+    display: flex;
+    justify-content: space-around;
+    background-color: #498a49;
+    font-family:'Trebuchet MS';
+    font-size: 27px;
+    color: #c79d6b;
+    margin-bottom: 0%;   
+  }
+
+  .maples {
+    margin-top: 0%;
+    display: flex;
+    flex-direction: column;
+  }
 
 </style>
